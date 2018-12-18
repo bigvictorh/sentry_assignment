c781c9f3542dbbfcade004b33f8238045edabbdf
Victor Hsiang <vhsiangyl@gmail.com>
2018-12-18 12:16:25 -0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { ErrorHandler } from "@angular/core";
+import * as Sentry from "@sentry/browser";
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'project';
+export class AppComponent implements ErrorHandler{
+  title = 'Test App';
+
+	handleError(error) {
+		Sentry.captureException(error.originalError || error);
+	    alert('An unexpected error occurred.');
+	        console.log(error);
+	}
+
+    
+    throwError() {
+    	try {throw new Error('an error');}
+    	catch(e) {
+    		// Sentry.captureException(e.orignalError || e);
+    		this.handleError(e);
+    	}
+    	throw(Error);
+    }
+		
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, ErrorHandler, Injectable } from "@angular/core";
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from './app.component';
+
+import { AppComponent } from "./app.component";
+
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+  dsn: "https://9960087589984225a0ffb214ce8088a8@sentry.io/1354806"
+});
+
+@Injectable()
+export class SentryErrorHandler implements ErrorHandler {
+  constructor() {}
+  handleError(error) {
+    Sentry.captureException(error.originalError || error);
+    throw error;
+  }
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [{ provide: ErrorHandler, useClass: SentryErrorHandler }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {}
